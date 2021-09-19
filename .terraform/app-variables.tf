1f3791dceed44357e1e0eb6d1e007a45b72e2ee7
Mohammad Ruhul Amin <mruhul@gmail.com>
2021-09-19 21:21:22 +1000

    initial commit

--- .terraform/app-variables.tf
+++ .terraform/app-variables.tf
@@ -22,12 +22,12 @@ variable "app_image" {
 
 variable "fargate_cpu" {
   description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-  default     = "128"
+  default     = "256"
 }
 
 variable "fargate_memory" {
   description = "Fargate instance memory to provision (in MiB)"
-  default     = "256"
+  default     = "512"
 }
 
 
