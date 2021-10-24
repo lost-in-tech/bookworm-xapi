b40de7ed64a01b5ccefe6ef43a30ea2378a1ffc7
Mohammad Ruhul Amin <mruhul@gmail.com>
2021-10-24 20:25:22 +1100

    wip...

--- .terraform/http-api.tf
+++ .terraform/http-api.tf
@@ -1,6 +1,12 @@
 resource "aws_apigatewayv2_api" "api" {
   name          = "api-${var.group}-${var.env}-${var.app_name}"
   protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = ["https://www.ruhul-amin.net"]
+    allow_methods = ["POST", "GET", "PUT", "OPTIONS"]
+    allow_headers = ["content-type"]
+    max_age       = 300
+  }
 }
 resource "aws_apigatewayv2_stage" "api" {
   api_id      = aws_apigatewayv2_api.api.id
