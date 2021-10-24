85370fd4875cb48b29190a7d51430433ac2eb714
Mohammad Ruhul Amin <mruhul@gmail.com>
2021-10-24 20:17:58 +1100

    wip...

--- .terraform/http-api.tf
+++ .terraform/http-api.tf
@@ -3,18 +3,16 @@ resource "aws_apigatewayv2_api" "api" {
   protocol_type = "HTTP"
 }
 resource "aws_apigatewayv2_stage" "api" {
-  api_id = aws_apigatewayv2_api.api.id
-  name   = var.env
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = var.env
+  auto_deploy = true
 }
 
 resource "aws_apigatewayv2_integration" "api" {
-  api_id           = aws_apigatewayv2_api.api.id
-  integration_type = "AWS_PROXY"
-
-  connection_type      = "INTERNET"
-  integration_method   = "ANY"
-  integration_uri      = aws_lambda_function.app.invoke_arn
-  passthrough_behavior = "WHEN_NO_MATCH"
+  api_id             = aws_apigatewayv2_api.api.id
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+  integration_uri    = aws_lambda_function.app.invoke_arn
 }
 
 resource "aws_apigatewayv2_route" "api" {
@@ -22,4 +20,18 @@ resource "aws_apigatewayv2_route" "api" {
   route_key = "ANY /${var.app_name}/{proxy+}"
 
   target = "integrations/${aws_apigatewayv2_integration.api.id}"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.api.name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.app.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
