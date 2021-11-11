17163241dd74634546f927486cee5b0c882e0ff7
Mohammad Ruhul Amin <mruhul@gmail.com>
2021-11-11 13:56:57 +1100

    adding cognito

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,3 +2,7 @@ output "cognito-client-secret" {
   value = aws_cognito_user_pool_client.main.client_secret
   sensitive = true
 }
+
+output "cognito-client-id" {
+  value = aws_cognito_user_pool_client.main.id
+}
