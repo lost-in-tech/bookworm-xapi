af195f168a7e803d1640db009ed0bf6898a05e66
Mohammad Ruhul Amin <mruhul@gmail.com>
2021-11-12 21:50:23 +1100

    registration

--- Bookworm.Xapi/Controllers/UsersController.cs
+++ Bookworm.Xapi/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System;
 using System.Threading.Tasks;
 
@@ -8,13 +9,36 @@ namespace Bookworm.Xapi.Controllers
   [Route("users")]
   public class UsersController : ControllerBase
   {
+        private readonly IOptions<UserPoolConfig> config;
+
+        public UsersController(IOptions<UserPoolConfig> config)
+    {
+            this.config = config;
+        }
+
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody]RegisterRequest request)
     {
-      return Ok(new {
-        ClientId = Environment.GetEnvironmentVariable("Auth__ClientId")
-      });
+      return Ok(config.Value);
     }
+  }
+
+  /*
+  Auth__Region = data.aws_region.current.name
+      Auth__ClientId = aws_cognito_user_pool_client.main.id
+      Auth__ClientSecret = aws_cognito_user_pool_client.main.client_secret
+      Auth__UserPoolId = aws_cognito_user_pool.main.id
+      Auth__Domain
+  
+  */
+
+  public class UserPoolConfig
+  {
+    public string Region { get; set; }
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+    public string UserPoolId { get; set; }
+    public string Domain { get; set; }
   }
 
   public class RegisterRequest
